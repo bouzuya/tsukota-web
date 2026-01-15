b48aead9cf3ada18206acade35c262d4553998f1
bouzuya <m@bouzuya.net>
2026-01-16 08:01:07 +0900

    Add FirestoreClient::new

--- backend/crates/firestore-client/src/lib.rs
+++ backend/crates/firestore-client/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub use googleapis_tonic_google_firestore_v1::google;
+
+pub struct FirestoreClient {
+    channel: tonic::transport::Channel,
+    credentials: google_cloud_auth::credentials::Credentials,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl FirestoreClient {
+    pub async fn new() -> Result<Self, Box<dyn std::error::Error + Send + Sync>> {
+        let channel = tonic::transport::Channel::from_static("https://firestore.googleapis.com")
+            .tls_config(tonic::transport::ClientTlsConfig::new().with_webpki_roots())?
+            .connect()
+            .await?;
+        let credentials = google_cloud_auth::credentials::Builder::default().build()?;
+        Ok(Self {
+            channel,
+            credentials,
+        })
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    async fn client(
+        &self,
+    ) -> Result<
+        google::firestore::v1::firestore_client::FirestoreClient<
+            tonic::service::interceptor::InterceptedService<
+                tonic::transport::Channel,
+                impl FnMut(tonic::Request<()>) -> Result<tonic::Request<()>, tonic::Status>,
+            >,
+        >,
+        Box<dyn std::error::Error + Send + Sync>,
+    > {
+        let cacheable_headers = self.credentials.headers(http::Extensions::new()).await?;
+        let header_map = match cacheable_headers {
+            google_cloud_auth::credentials::CacheableResource::New { data, .. } => data,
+            google_cloud_auth::credentials::CacheableResource::NotModified => {
+                return Err("NotModified response without cached data".into());
+            }
+        };
+        let metadata = tonic::metadata::MetadataMap::from_headers(header_map);
+
+        let firestore_client =
+            google::firestore::v1::firestore_client::FirestoreClient::with_interceptor(
+                self.channel.clone(),
+                move |mut request: tonic::Request<()>| {
+                    for key_and_value in metadata.iter() {
+                        match key_and_value {
+                            tonic::metadata::KeyAndValueRef::Ascii(key, value) => {
+                                request.metadata_mut().insert(key, value.clone());
+                            }
+                            tonic::metadata::KeyAndValueRef::Binary(key, value) => {
+                                request.metadata_mut().insert_bin(key, value.clone());
+                            }
+                        }
+                    }
+                    Ok(request)
+                },
+            );
+
+        Ok(firestore_client)
     }
 }
