17694661397b5e27ca4e1d7db3645c9e04365d64
bouzuya <m@bouzuya.net>
2026-01-16 22:05:50 +0900

    Improve FirestoreClient

--- backend/crates/firestore-client/src/lib.rs
+++ backend/crates/firestore-client/src/lib.rs
@@ -2,24 +2,49 @@ pub use firestore_path as path;
 pub use googleapis_tonic_google_firestore_v1::google;
 
 #[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("credentials error: {0}")]
-    Credentials(#[from] google_cloud_auth::errors::CredentialsError),
+#[error(transparent)]
+pub struct Error(#[from] E);
 
-    #[error("build credentials error: {0}")]
-    BuildCredentials(#[from] google_cloud_auth::build_errors::Error),
+#[derive(Debug, thiserror::Error)]
+enum E {
+    #[error("build credentials")]
+    BuildCredentials(#[source] google_cloud_auth::build_errors::Error),
 
-    #[error("transport error: {0}")]
-    Transport(#[from] tonic::transport::Error),
+    #[error("connect")]
+    Connect(#[source] tonic::transport::Error),
 
-    #[error("grpc status error: {0}")]
-    Status(#[from] tonic::Status),
+    #[error("create document")]
+    CreateDocument(#[source] tonic::Status),
 
-    #[error("deserialize error: {0}")]
-    Deserialize(#[from] serde_firestore_value::Error),
+    #[error("credentials")]
+    Credentials(#[source] google_cloud_auth::errors::CredentialsError),
 
     #[error("credentials not modified")]
     CredentialsNotModified,
+
+    #[error("deserialize")]
+    Deserialize(#[source] serde_firestore_value::Error),
+
+    #[error("get document")]
+    GetDocument(#[source] tonic::Status),
+
+    #[error("invalid value for document fields")]
+    InvalidValueForDocumentFields,
+
+    #[error("list documents")]
+    ListDocuments(#[source] tonic::Status),
+
+    #[error("serialize")]
+    Serialize(#[source] serde_firestore_value::Error),
+
+    #[error("set document")]
+    SetDocument(#[source] tonic::Status),
+
+    #[error("tls config")]
+    TlsConfig(#[source] tonic::transport::Error),
+
+    #[error("update document")]
+    UpdateDocument(#[source] tonic::Status),
 }
 
 #[derive(Clone)]
@@ -32,15 +57,64 @@ pub struct FirestoreClient {
 impl FirestoreClient {
     pub async fn connect(database_name: path::DatabaseName) -> Result<Self, Error> {
         let channel = tonic::transport::Channel::from_static("https://firestore.googleapis.com")
-            .tls_config(tonic::transport::ClientTlsConfig::new().with_webpki_roots())?
+            .tls_config(tonic::transport::ClientTlsConfig::new().with_webpki_roots())
+            .map_err(E::TlsConfig)?
             .connect()
-            .await?;
-        let credentials = google_cloud_auth::credentials::Builder::default().build()?;
+            .await
+            .map_err(E::Connect)?;
+        let credentials = google_cloud_auth::credentials::Builder::default()
+            .build()
+            .map_err(E::BuildCredentials)?;
         Ok(Self {
             channel,
             credentials,
             database_name,
         })
+    }
+
+    pub async fn create_document(
+        &self,
+        document_path: path::DocumentPath,
+        value: google::firestore::v1::Value,
+    ) -> Result<google::firestore::v1::Document, Error> {
+        let document_name = self
+            .database_name
+            .doc(document_path)
+            .expect("DatabaseName::doc(DocumentPath) to be valid");
+        let mut client = self.client().await?;
+        let google::firestore::v1::CreateDocumentRequest {
+            collection_id: _,
+            document: _,
+            document_id: _,
+            mask,
+            parent: _,
+        } = Default::default();
+        let request = google::firestore::v1::CreateDocumentRequest {
+            collection_id: document_name.collection_id().to_string(),
+            document_id: document_name.document_id().to_string(),
+            mask,
+            parent: document_name
+                .parent_document_name()
+                .map(|parent| parent.to_string())
+                .unwrap_or_else(|| document_name.root_document_name().to_string()),
+            document: Some(google::firestore::v1::Document {
+                name: String::new(),
+                fields: match value {
+                    google::firestore::v1::Value {
+                        value_type:
+                            Some(google::firestore::v1::value::ValueType::MapValue(map_value)),
+                    } => Ok(map_value.fields),
+                    _ => Err(E::InvalidValueForDocumentFields),
+                }?,
+                create_time: None,
+                update_time: None,
+            }),
+        };
+        Ok(client
+            .create_document(request)
+            .await
+            .map_err(E::CreateDocument)?
+            .into_inner())
     }
 
     pub async fn deserialize<T>(
@@ -55,16 +129,16 @@ impl FirestoreClient {
                 google::firestore::v1::MapValue { fields },
             )),
         };
-        Ok(serde_firestore_value::from_value(&value)?)
+        Ok(serde_firestore_value::from_value(&value).map_err(E::Deserialize)?)
     }
 
     pub async fn get_document(
         &self,
-        document_path: &path::DocumentPath,
+        document_path: path::DocumentPath,
     ) -> Result<Option<google::firestore::v1::Document>, Error> {
         let document_name = self
             .database_name
-            .doc(document_path.clone())
+            .doc(document_path)
             .expect("DatabaseName::doc(DocumentPath) to be valid");
         let mut client = self.client().await?;
         let google::firestore::v1::GetDocumentRequest {
@@ -81,18 +155,18 @@ impl FirestoreClient {
         match result {
             Ok(response) => Ok(Some(response.into_inner())),
             Err(status) if status.code() == tonic::Code::NotFound => Ok(None),
-            Err(status) => Err(Error::Status(status)),
+            Err(status) => Err(E::GetDocument(status))?,
         }
     }
 
     pub async fn list_documents(
         &self,
-        collection_path: &path::CollectionPath,
+        collection_path: path::CollectionPath,
         page_token: Option<String>,
     ) -> Result<google::firestore::v1::ListDocumentsResponse, Error> {
         let collection_name = self
             .database_name
-            .collection(collection_path.clone())
+            .collection(collection_path)
             .expect("DatabaseName::collection(CollectionPath) to be valid");
         let mut client = self.client().await?;
         let google::firestore::v1::ListDocumentsRequest {
@@ -106,7 +180,7 @@ impl FirestoreClient {
             show_missing,
         } = Default::default();
         let request = google::firestore::v1::ListDocumentsRequest {
-            collection_id: collection_path.collection_id().to_string(),
+            collection_id: collection_name.collection_id().to_string(),
             consistency_selector,
             mask,
             order_by,
@@ -118,7 +192,102 @@ impl FirestoreClient {
                 .unwrap_or_else(|| self.database_name.root_document_name().to_string()),
             show_missing,
         };
-        Ok(client.list_documents(request).await?.into_inner())
+        Ok(client
+            .list_documents(request)
+            .await
+            .map_err(E::ListDocuments)?
+            .into_inner())
+    }
+
+    pub fn serialize<T>(&self, value: &T) -> Result<google::firestore::v1::Value, Error>
+    where
+        T: serde::Serialize,
+    {
+        Ok(serde_firestore_value::to_value(value).map_err(E::Serialize)?)
+    }
+
+    pub async fn set_document(
+        &self,
+        document_path: path::DocumentPath,
+        value: google::firestore::v1::Value,
+    ) -> Result<google::firestore::v1::Document, Error> {
+        let document_name = self
+            .database_name
+            .doc(document_path)
+            .expect("DatabaseName::doc(DocumentPath) to be valid");
+        let mut client = self.client().await?;
+        let google::firestore::v1::UpdateDocumentRequest {
+            current_document: _,
+            document: _,
+            mask,
+            update_mask,
+        } = Default::default();
+        let request = google::firestore::v1::UpdateDocumentRequest {
+            current_document: None,
+            mask,
+            update_mask,
+            document: Some(google::firestore::v1::Document {
+                create_time: None,
+                fields: match value {
+                    google::firestore::v1::Value {
+                        value_type:
+                            Some(google::firestore::v1::value::ValueType::MapValue(map_value)),
+                    } => Ok(map_value.fields),
+                    _ => Err(E::InvalidValueForDocumentFields),
+                }?,
+                name: document_name.to_string(),
+                update_time: None,
+            }),
+        };
+        Ok(client
+            .update_document(request)
+            .await
+            .map_err(E::SetDocument)?
+            .into_inner())
+    }
+
+    pub async fn update_document(
+        &self,
+        document_path: path::DocumentPath,
+        value: google::firestore::v1::Value,
+    ) -> Result<google::firestore::v1::Document, Error> {
+        let document_name = self
+            .database_name
+            .doc(document_path)
+            .expect("DatabaseName::doc(DocumentPath) to be valid");
+        let mut client = self.client().await?;
+        let google::firestore::v1::UpdateDocumentRequest {
+            current_document: _,
+            document: _,
+            mask,
+            update_mask,
+        } = Default::default();
+        let request = google::firestore::v1::UpdateDocumentRequest {
+            current_document: Some(google::firestore::v1::Precondition {
+                condition_type: Some(google::firestore::v1::precondition::ConditionType::Exists(
+                    true,
+                )),
+            }),
+            mask,
+            update_mask,
+            document: Some(google::firestore::v1::Document {
+                create_time: None,
+                fields: match value {
+                    google::firestore::v1::Value {
+                        value_type:
+                            Some(google::firestore::v1::value::ValueType::MapValue(map_value)),
+                    } => Ok(map_value.fields),
+                    _ => Err(E::InvalidValueForDocumentFields),
+                }?,
+                name: document_name.to_string(),
+                update_time: None,
+            }),
+        };
+        Ok(client
+            .update_document(request)
+            .await
+            .map_err(E::UpdateDocument)?
+            .into_inner())
     }
 
     async fn client(
@@ -132,11 +301,15 @@ impl FirestoreClient {
         >,
         Error,
     > {
-        let cacheable_headers = self.credentials.headers(http::Extensions::new()).await?;
+        let cacheable_headers = self
+            .credentials
+            .headers(http::Extensions::new())
+            .await
+            .map_err(E::Credentials)?;
         let header_map = match cacheable_headers {
             google_cloud_auth::credentials::CacheableResource::New { data, .. } => data,
             google_cloud_auth::credentials::CacheableResource::NotModified => {
-                return Err(Error::CredentialsNotModified);
+                return Err(E::CredentialsNotModified)?;
             }
         };
         let metadata = tonic::metadata::MetadataMap::from_headers(header_map);
