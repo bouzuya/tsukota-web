7f5046cbbba07fb959468a94c0f614acf9139750
bouzuya <m@bouzuya.net>
2026-01-21 07:53:39 +0900

    Fix to use FirebaseEventStore

--- backend/crates/infra/src/lib.rs
+++ backend/crates/infra/src/lib.rs
@@ -2,6 +2,8 @@ mod firestore_event_store;
 mod in_memory_event_store;
 mod in_memory_projection;
 
+pub use firestore_client::FirestoreClient;
+pub use firestore_client::path::DatabaseName;
 pub use firestore_event_store::FirestoreEventStore;
 pub use in_memory_event_store::InMemoryEventStore;
 pub use in_memory_projection::InMemoryProjection;
--- backend/crates/main/src/main.rs
+++ backend/crates/main/src/main.rs
@@ -1,11 +1,16 @@
 use api::AppState;
+use infra::DatabaseName;
+use infra::FirestoreClient;
+use infra::FirestoreEventStore;
 use infra::InMemoryEventStore;
 use infra::InMemoryProjection;
 
 #[tokio::main]
 async fn main() {
     // Initialize infrastructure components
-    let event_store = InMemoryEventStore::new();
+    // let event_store = InMemoryEventStore::new();
+    let event_store =
+        FirestoreEventStore::new(FirestoreClient::connect_with_emulator().await.unwrap());
     let projection = InMemoryProjection::with_events(event_store.events());
 
     // Create application state with DI
