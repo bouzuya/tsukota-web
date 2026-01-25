da6090bfa60cd07ef844492c5c3e8524be7b7526
bouzuya <m@bouzuya.net>
2026-01-25 09:27:49 +0900

    Add DeviceId

--- backend/crates/domain/src/account/value_objects.rs
+++ backend/crates/domain/src/account/value_objects.rs
@@ -1,5 +1,6 @@
 mod account_id;
 mod category_id;
+mod device_id;
 mod transaction_id;
 mod user_id;
 
@@ -7,6 +8,8 @@ pub use account_id::AccountId;
 pub use account_id::ParseAccountIdError;
 pub use category_id::CategoryId;
 pub use category_id::ParseCategoryIdError;
+pub use device_id::DeviceId;
+pub use device_id::ParseDeviceIdError;
 pub use transaction_id::ParseTransactionIdError;
 pub use transaction_id::TransactionId;
 pub use user_id::ParseUserIdError;
